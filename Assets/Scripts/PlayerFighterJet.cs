aafc4a876b6c3be8b6c86128e9470d7395fbfb35
Veman Jadhav <vemancraft@gmail.com>
2025-06-15 17:18:42 +0530

    Reverted to old input system and added code for manipulating two vectors.
    Not finished yet.

--- Assets/Scripts/PlayerFighterJet.cs
+++ Assets/Scripts/PlayerFighterJet.cs
@@ -2,13 +2,33 @@ using UnityEngine;
 
 public class PlayerFighterJet : MonoBehaviour
 {
+    [HideInInspector] public Vector3 v_main;
+    /*[HideInInspector]*/ public Vector3 v_travel;
+
+    public float f_travelVectorMagnitude = 10f;
+
     private void Start()
     {
-        
+        v_main = transform.forward * 10f;
+        v_travel = transform.forward * 10f;
     }
 
     private void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        //Debug.Log("X: " + x + ", Y: " + z);
+
+        v_travel.Normalize();
+        v_travel.x = x * f_travelVectorMagnitude;
+        //v_travel.y = y * f_travelVectorMagnitude;
+        v_travel.z *= f_travelVectorMagnitude;
+
+        //Debug.DrawRay(transform.position, v_main, Color.blue);
+        Debug.DrawRay(transform.position, v_travel, Color.red);
+
+        // Lerping the plane's vector with the travel vector.
+        transform.rotation = Quaternion.FromToRotation(transform.forward, v_travel);
     }
 }
