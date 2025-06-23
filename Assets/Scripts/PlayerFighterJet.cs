4cf31ecf51f54d4fc82a76510697a93832aa72cf
Veman Jadhav <vemancraft@gmail.com>
2025-06-24 00:02:52 +0530

    Second movement option using "transform.Rotate()"
    The first one felt good but it wasn't snappy enough. Decided to make use of a second option where it didn't make use of the vector method at all and just the normal "transform.Rotate".

--- Assets/Scripts/PlayerFighterJet.cs
+++ Assets/Scripts/PlayerFighterJet.cs
@@ -1,6 +1,5 @@
 /* My attempt at writing the fighter jet movement code in the style of Quake (1997)'s player movement logic. */
 
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -11,20 +10,17 @@ public class PlayerFighterJet : MonoBehaviour
     [Header("Vector-based jet rotation")]
     public Vector3 v_travel;                        // The travel vector that directs the gameobject's main velocity vector.
     public Vector3 v_roll;                          // Second vector that behaves like 'v_travel' but only facilitates rolling along the Z-axis.
-    private Vector3 v_forwardVisualizer;            // Visualizes the gameobject's forward (Z) vector.
-    private Vector3 v_rightVisualizer;              // Visualizes the gameobject's right (X) vector.
-    private Vector3 v_targetDirection;              // The normalized direction between the forward vector and the travel vector.
     private Vector3 v_main;                         // The forward vector with a specified magnitude.
     private Vector3 v_lateral;                      // The lateral vector with a specified magnitude.
     private Vector3 v_fwdTorqueAxis;
+    private Vector3 v_rgtTorqueAxis;
 
     public float f_travelVectorMagnitude = 10f;
     public float f_torqueMultiplier = 0.05f;
     private float f_fwdToTravelAngle;
-
-    private Vector3 v_rollDirection;
     private float f_rgtToTravelAngle;
-    private Vector3 v_rgtTorqueAxis;
+
+    private float f_turningConstant = 2.8125f;
 
     [Header("Jet throttle")]
     public float f_throttleIncrement = 0.2f;
@@ -37,8 +33,12 @@ public class PlayerFighterJet : MonoBehaviour
     public float smoothTime = 0.3f;
     private Vector3 camRefVelocity = Vector3.zero;
 
-    [Header("Debug values")]
-    [SerializeField] private float f_travelMag;
+    [Header("DEBUG")]
+    [SerializeField] private bool b_visualizeVectors;
+    [SerializeField] private MovementOptions e_movementOptions;
+    private enum MovementOptions { RigidbodyBased, BruteForce };
+    private Vector3 v_forwardVisualizer;            // Visualizes the gameobject's forward (Z) vector.
+    private Vector3 v_rightVisualizer;              // Visualizes the gameobject's right (X) vector.
 
     private void Start()
     {
@@ -51,16 +51,13 @@ public class PlayerFighterJet : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         float z = Input.GetAxis("Roll");
-        float t = Input.GetAxis("Throttle");
+        float t = Input.GetAxisRaw("Throttle");
 
         if (cameraPlaceholder && cameraLookAtTarget) CameraUpdate();
-        VisualizeVectors();
+        if (b_visualizeVectors) VisualizeVectors();
 
         // Throttle controls.
-        //if (Input.GetKey(KeyCode.Keypad8)) f_throttle += f_throttleIncrement;
-        //if (Input.GetKey(KeyCode.Keypad5)) f_throttle -= f_throttleIncrement;
         f_throttle += f_throttleIncrement * t;
-        //Mathf.Clamp(f_throttle, 0f, 500f);              // Clamping values below 0f doesn't seem to work. Needs a fix!
         if (f_throttle >= 5000f) f_throttle = 5000f;
         if (f_throttle <= 0f) f_throttle = 0f;
 
@@ -70,34 +67,58 @@ public class PlayerFighterJet : MonoBehaviour
         v_travel = (transform.right * x + transform.up * y + transform.forward).normalized * f_travelVectorMagnitude;
         v_roll = (transform.right * z + transform.up).normalized * f_travelVectorMagnitude;
 
-        v_targetDirection = (v_travel - v_main).normalized;
-        v_fwdTorqueAxis = Vector3.Cross(transform.forward, v_targetDirection);
-        f_fwdToTravelAngle = Vector3.Angle(transform.forward, v_targetDirection);
+        switch (e_movementOptions)
+        {
+            case MovementOptions.RigidbodyBased:
+                RigidBodyBasedAxesMovement();
+                break;
 
-        v_rollDirection = (v_roll - v_lateral).normalized;
-        v_rgtTorqueAxis = Vector3.Cross(transform.up, v_rollDirection);
-        f_rgtToTravelAngle = Vector3.Angle(transform.up, v_rollDirection);
-
-        //f_fwdToTravelAngle = VectorAngle(v_fwdTorqueAxis, v_travel, v_main, transform.forward);
-
-        // Output v_travel's magnitude in the editor. Can be commented.
-        f_travelMag = v_travel.magnitude;
-
-        // Output the angle between the forward vector and v_travel.
-        //f_fwdToTravelAngle = Vector3.Angle(transform.forward, v_travel);
+            case MovementOptions.BruteForce:
+                BruteForceAxesMovement(x, y, z);
+                break;
+        }
     }
 
-    private void FixedUpdate()
+    private void RigidBodyBasedAxesMovement()
     {
-        // Throttle
-        rb.AddForce(f_throttle * f_maxThrust * transform.forward);
+        Vector3 targetDir = (v_travel - v_main).normalized;
+        v_fwdTorqueAxis = Vector3.Cross(transform.forward, targetDir);
+        f_fwdToTravelAngle = Vector3.Angle(transform.forward, targetDir);
 
-        // Rotating the jet along the X and Y axes for now. (TO DO: extra dampening for the torque forces.)
-        rb.AddTorque(f_fwdToTravelAngle * f_torqueMultiplier * v_fwdTorqueAxis, ForceMode.Acceleration);
-
-        // Rotating the jet on the Z axis to simulate roll.
-        rb.AddTorque(f_rgtToTravelAngle * f_torqueMultiplier * v_rgtTorqueAxis, ForceMode.Acceleration);
+        Vector3 rollDir = (v_roll - v_lateral).normalized;
+        v_rgtTorqueAxis = Vector3.Cross(transform.up, rollDir);
+        f_rgtToTravelAngle = Vector3.Angle(transform.up, rollDir);
     }
+
+    private void BruteForceAxesMovement(float _yaw, float _pitch, float _roll)
+    {
+        //Vector3 yawPitch = Vector3.RotateTowards(transform.forward, v_travel, Time.deltaTime, 0f);
+
+        /* NOTE FOR LINE 99:
+         * This is always glitching for some reason and upon further inspection, it's most likely because of how
+         * 'Vector3.RotateTowards' behaves after 90 radians. Need to either find a fix for this or an alternative.
+         * 'transform.LookAt()' doesn't work either.*/
+        //Vector3 roll = Vector3.RotateTowards(transform.up, v_roll, Time.deltaTime, 0f);
+
+        //transform.rotation = Quaternion.LookRotation(yawPitch);
+        //transform.rotation = Quaternion.LookRotation(roll);
+
+        transform.Rotate(Vector3.up, f_turningConstant * _yaw, Space.Self);
+        transform.Rotate(Vector3.right, -f_turningConstant * _pitch, Space.Self);
+        transform.Rotate(Vector3.forward, -f_turningConstant * _roll, Space.Self);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    // Throttle
+    //    rb.AddForce(f_throttle * f_maxThrust * transform.forward);
+
+    //    // Rotating the jet along the X and Y axes for now. (TO DO: extra dampening for the torque forces.)
+    //    rb.AddTorque(f_fwdToTravelAngle * f_torqueMultiplier * v_fwdTorqueAxis, ForceMode.Acceleration);
+
+    //    // Rotating the jet on the Z axis to simulate roll.
+    //    rb.AddTorque(f_rgtToTravelAngle * f_torqueMultiplier * v_rgtTorqueAxis, ForceMode.Acceleration);
+    //}
 
     private void CameraUpdate()
     {
@@ -119,13 +140,5 @@ public class PlayerFighterJet : MonoBehaviour
         Debug.DrawRay(transform.position, v_rightVisualizer, Color.red);
         Debug.DrawRay(transform.position, v_travel, Color.magenta);
         Debug.DrawRay(transform.position, v_roll, Color.cyan);
-    }
-
-    private float VectorAngle(Vector3 torqueAxis, Vector3 action, Vector3 main, Vector3 dirAxis)
-    {
-        Vector3 vectorDir = (action - main).normalized;
-        torqueAxis = Vector3.Cross(dirAxis, vectorDir);
-        float angle = Vector3.Angle(dirAxis, vectorDir);
-        return angle;
     }
 }
