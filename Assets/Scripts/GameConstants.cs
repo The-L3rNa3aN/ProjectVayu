4911bcc5e37a7ac7cb68ce8e25b8089fee242b8b
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2025-08-01 18:48:43 +0530

    Create GameConstants.cs
    Constants for accessing it globally.

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,5 @@
+public static class GameConstants
+{
+    public const float F_GRAVITY = 9.81F;                           /// Gravitational acceleration
+    public const float F_KM2KNOT = 0.54F;                           /// Kilometer to knot (nautical mile)
+}
